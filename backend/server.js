c6848fe7c9b382dc22e426dd168759493ffad627
Gopasri <gopasri11@gmail.com>
2026-05-20 20:18:35 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ const connectDB =require("./config/db.js");
 const app = express();
 console.log("JWT SECRET:", process.env.JWT_SECRET);
 
-app.use(cors({ origin: "http://localhost:5173"}));
+app.use(cors({
+  origin: "https://notesapp-two-delta.vercel.app/",
+  methods: ["GET", "POST", "PUT", "DELETE"]
+}));
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
