717959da4050ddbc0cfd470e01a7b943632f6917
gopasri11-dev <gopasri11@gmail.com>
2026-05-20 20:39:52 +0530

    Add CORS configuration for frontend access

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ const connectDB =require("./config/db.js");
 const app = express();
 console.log("JWT SECRET:", process.env.JWT_SECRET);
 
+
 app.use(cors({
 origin: "https://notesapp-two-delta.vercel.app/",
 methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
