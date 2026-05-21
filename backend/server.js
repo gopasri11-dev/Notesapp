4ebb8fd5c5869350c23f6d3026d84d57e9565840
Gopasri <gopasri11@gmail.com>
2026-05-21 14:12:06 +0530

    single-server-deploymentnotes

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.static(
   path.join(__dirname, "../frontend/dist")
 ));
 
-app.get("*", (req, res) => {
+app.use( (req, res) => {
   res.sendFile(
     path.join(__dirname, "../frontend/dist/index.html")
   )
